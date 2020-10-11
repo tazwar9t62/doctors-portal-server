7e21b5c0f57de6b714c88e786ef44ec3eee63b8b
Tazwar <tazwar9t6@gmail.com>
2020-10-11 16:40:28 +0600

    test

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+
+const app = express();
+
+const uri = "mongodb+srv://9t6:livelovedie@cluster0.dupvj.mongodb.net/doctorsPortal?retryWrites=true&w=majority";
+app.use(cors());
+app.use(bodyParser.json());
+const port = 5000;
+
+
+const client = new MongoClient(
+    uri,
+    { useUnifiedTopology: true },
+    { useNewUrlParser: true },
+    { connectTimeoutMS: 30000 },
+    { keepAlive: 1 }
+  );
+  client.connect(err => {
+    const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+
+app.post("/addAppointment" , (req, res) => {
+    const appointment = req.body;
+    console.log(appointment);
+    appointmentCollection.insertOne(appointment)
+    .then((result)=>{
+        res.send(result.insertedCount > 0)
+    })
+    .catch(error => { throw error})
+})
+});
+  app.get("/", (req, res) => {
+    res.send("Hello Tazwar , your server working!");
+  });
+  
+  app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
